d3960d59d05fff0c3943ed559c3fa6c978bd7acf
Elizabeth Collins <escollins27@gmail.com>
2014-09-29 15:23:14 -0400

    Users can edit galleries

--- app/controllers/galleries_controller.rb
+++ app/controllers/galleries_controller.rb
@@ -17,9 +17,16 @@ class GalleriesController < ApplicationController
     @gallery = Gallery.find(params[:id])
   end
 
+  def update
+    gallery = Gallery.find(params[:id])
+    gallery.update(name: params[:gallery][:name],
+                   description: params[:gallery][:description])
+    redirect_to "/"
+  end
+
   def create
     Gallery.create(name: params[:gallery][:name],
-                   description:params[:gallery][:description]
+                   description: params[:gallery][:description]
                   )
     redirect_to "/"
   end
