5e165c98cf0b71b4823ea44e2d881a319e3db1df
Elizabeth Collins <escollins27@gmail.com>
2014-10-02 16:46:18 -0400

    Scope galleries and images to current users

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,42 +1,45 @@
 class ImagesController < ApplicationController
- def show
-   @gallery = Gallery.find(params[:gallery_id])
-   @image = @gallery.images.find(params[:id])
- end
- def new
-   @gallery = Gallery.find(params[:gallery_id])
-   @image = @gallery.images.new
+  def show
+    @gallery = load_gallery_from_url
+    @image = @gallery.images.find(params[:id])
+  end
+  def new
+    @gallery = load_gallery_from_url
+    @image = @gallery.images.new
 
- end
- def create
-   @gallery = Gallery.find(params[:gallery_id])
-   @image = @gallery.images.new(image_params)
-   if @image.save
-   redirect_to gallery_path(@gallery)
-   else
-   render :new
-   end
- end
+  end
+  def create
+    @gallery = load_gallery_from_url
+    @image = @gallery.images.new(image_params)
+    if @image.save
+      redirect_to gallery_path(@gallery)
+    else
+      render :new
+    end
+  end
 
- def edit
-   @gallery = Gallery.find(params[:gallery_id])
-   @image = @gallery.images.find(params[:id])
- end
+  def edit
+    @gallery = load_gallery_from_url
+    @image = @gallery.images.find(params[:id])
+  end
 
-def update
-  @gallery = Gallery.find(params[:gallery_id])
-  @image = @gallery.images.find(params[:id])
+  def update
+    @gallery = load_gallery_from_url
+    @image = @gallery.images.find(params[:id])
 
     if @image.update(image_params)
-    redirect_to gallery_image_path(@gallery, @image)
+      redirect_to gallery_image_path(@gallery, @image)
     else
-     render :edit
+      render :edit
     end
-end
- private
- def image_params
-   params
-     .require(:image)
-     .permit(:name,:url)
- end
+  end
+  private
+  def image_params
+    params
+      .require(:image)
+      .permit(:name,:url)
+  end
+  def load_gallery_from_url
+    current_user.galleries.find(params[:gallery_id])
+  end
 end
