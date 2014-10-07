18705a196747e0cec8dbd7942ac749f723e4d30b
Elizabeth Collins <escollins27@gmail.com>
2014-10-07 17:41:47 -0400

    Member can join and leave groups

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,12 @@
 class User < ActiveRecord::Base
   has_many :galleries
-  has_many :groups_memberships, foreign_key: "member_id"
+  has_many :group_memberships, foreign_key: "member_id"
   has_many :groups, through: :group_memberships
 
   validates :email, presence: true, uniqueness: true
   validates :password_digest, presence: true
+
+  def membership_for(group)
+    self.group_memberships.where(group_id: group.id).first
+  end
 end
